99751a4c33d52d403d3272849bf073f9e64e8270
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2024-12-22 10:03:18 +0700

    `feat`: adjusted threshold, add extension filter on image upload

--- app_interface.py
+++ app_interface.py
@@ -1,25 +1,30 @@
 from flask import Flask, request, render_template, jsonify
-# from keras._tf_keras.keras.models import load_model
 import tensorflow as tf
 from tensorflow import keras
-from keras._tf_keras.keras.models import load_model
 from keras._tf_keras.keras.preprocessing import image
 import numpy as np
 import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 MODEL_PATH = './models/bansos_model_ai.keras'
-model = load_model(MODEL_PATH)
+model = keras.models.load_model(MODEL_PATH)
 
 UPLOAD_FOLDER = 'uploads/'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+# Fungsi untuk memeriksa ekstensi file
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 def prepare_image(img_path):
     img = image.load_img(img_path, target_size=(224, 224))
-    img_array = image.img_to_array(img) 
-    img_array = np.expand_dims(img_array, axis=0) 
-    img_array = img_array / 255.0 
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0
     return img_array
 
 @app.route('/')
@@ -35,18 +40,23 @@ def predict():
     
     if file.filename == '':
         return jsonify({'error': 'File tidak ditemukan'}), 400
-
-    img_path = os.path.join(str(UPLOAD_FOLDER), str(file.filename)) 
+    
+    # Memeriksa apakah ekstensi file valid
+    if not allowed_file(file.filename):
+        return jsonify({'error': 'Ekstensi file tidak valid. Harap unggah file gambar (PNG, JPG, JPEG)'}), 400
+    
+    # Menyimpan file dengan nama aman
+    file_name = secure_filename(file.filename)
+    img_path = os.path.join(UPLOAD_FOLDER, file_name)
     file.save(img_path)
     
     img_array = prepare_image(img_path)
     prediction = model.predict(img_array)
-    # threshold = 0.3 
-    # prediction = (prediction > threshold).astype(int)
 
-    print(f'Prediction[0]: {prediction[0]}')
+    print(f'Raw Prediction: {prediction[0]}') 
 
-    eligibility = 'Eligible' if prediction[0] < 0.3 else 'Not Eligible'
+    threshold = 0.8  
+    eligibility = 'Eligible' if prediction[0] >= threshold else 'Not Eligible'
 
     return jsonify({'prediction': eligibility, 'confidence': float(prediction[0])})
 
